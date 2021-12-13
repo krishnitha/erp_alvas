b105e4a21b9dfaacace010bffe3063cc78273988
iishaii <ishahegde50@gmail.com>
2021-12-13 14:24:44 +0530

    init

--- Fac_Leave_Management/casualleave.php
+++ Fac_Leave_Management/casualleave.php
@@ -1,5 +1,5 @@
 <?php
-// require_once "../config.php";
+require_once "../config.php";
 include("../template/fac-auth.php");
 $con=$link;
 
---
+++ student_leave_management/hod_leaveApprove.php
@@ -0,0 +1,26 @@
+<?php
+require_once "../config.php";
+$con=$link;
+include("../template/student_sidebar.php");
+?>
+            <div>
+                
+                <h5 style="text-align:center">Approval Details</h5><br>
+                <table class="table table-responsive table-borderless">
+                    <tbody>
+                        <tr>
+                            <th>USN</th>
+                            <th>Name</th>
+                            <th>Semester</th>
+                            <th>Section</th>
+                            <th>View</th>
+                        </tr>
+                        
+            </div>
+                               
+                    </tbody>
+                </table>
+
+
+
+               
