f4083f4c0c4523827368afead641640abc73ee11
iishaii <ishahegde50@gmail.com>
2022-05-07 11:44:09 +0530

    scl

--- Fac_Leave_Management/sclleaveapply.php
+++ Fac_Leave_Management/sclleaveapply.php
@@ -16,8 +16,8 @@ include("../template/sidebar-fac.php");
             }
         </style>
         <h4 style="text-align:center">Apply for SCL</h4><br>
-        <div style="margin-left:20%;margin-right:20%">
-      		<form action="upload_casualleave.php" method="POST">
+            <div style="margin-left:20%;margin-right:20%">
+      		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
         		<table   class="table table-responsive table-borderless">
         			<tr>
         				<th></th>
@@ -28,23 +28,10 @@ include("../template/sidebar-fac.php");
         				<td>Reason<br></td>
                         <td></td>
                         <td> 
-                            <input type = "text" name="reason" class = "form-control" id = "reason" placeholder = "Enter the Reason for leave" required>   
+                            <input type = "text" name="reason" class = "form-control" id = "reason" placeholder = "Enter the Reason" required>   
         				</td>
         			</tr>
-                    <tr>
-        				<td>Number of days<br></td>
-                        <td></td>
-                        <td> 
-                            <input type="number" name="r_clear" class="form-control" min="0" required> 
-        				</td>
-        			</tr>
-                    <tr>
-                        <td>Date<br></td>
-                        <td></td>
-                        <td> 
-                            <input type = "date" name="cdate" class="form-control" required>
-                        </td>
-                    </tr>
+
                     <tr>
                         <td> From <br></td>
                         <td></td>
@@ -62,17 +49,72 @@ include("../template/sidebar-fac.php");
                         <td>Upload Document<br></td>
                         <td></td>
                         <td>
-                            <input type="file" name="ufile" id="actual-btn" hidden required/>
+                            <input type="file" name="fileupload" id="actual-btn" hidden required/>
                             <label class="label2" for="actual-btn">Choose File</label>
                             <input type="hidden" name="MAX_FILE_SIZE" required value="100000">
                             <span id="file-chosen">No file chosen</span>
                             <input type="hidden" name="MAX_FILE_SIZE" value="100000">
+                            <?php
+                            if(isset($_POST["Submit"]))
+                            {
+                                $target_dir="../leave_doc/scl_doc/";
+                                $filename=$_FILES["fileupload"]["name"];
+                                $tmpname=$_FILES["fileupload"]["tmp_name"];
+                                $filetype=$_FILES["fileupload"]["type"];
+                                $errors=[];
+                                $fileextensions=["pdf","jpeg","jpg","png"];
+                                $arr=explode(".",$filename);
+                                $ext=strtolower(end($arr));
+                                $uploadpath=$target_dir.basename($filename);
+                                if(!in_array($ext,$fileextensions))
+                                {
+                                    $errors[]="Sorry, only JPG, JPEG, PNG & PDF files are allowed.";
+                                }
+                                if (file_exists($target_dir . $filename)) {
+                                    $errors[]= "Sorry, file already exists.";
+                                }
+                                if(empty($errors))
+                                {
+                                    if(move_uploaded_file($tmpname,$uploadpath))
+                                    {
+                                        $s='select * from faculty_details where faculty_name="' . $_SESSION["username"] . '"';
+                                        $res = $link->query($s);
+                                        $res = mysqli_fetch_assoc($res);
+                                        $reason = $_POST["reason"];
+                                        $date = date('Y-m-d');
+                                        $from = $_POST["from_date"];
+                                        $to = $_POST["to_date"];
+                                        $que = "insert into faculty_scl(faculty_name,reason,applied_date,from_date,to_date,doc_name) values (\"" . $_SESSION['username'] . "\",
+                                        \"" . $reason . "\",\"" . $date . "\",\"" . $from . "\",\"" . $to . "\",\"" . $uploadpath . "\")";
+                                        $result = $con->query($que);
+                                        echo '<script>window.location.replace("../fac_leave_management/sclleave.php");</script>';
+                                    }
+                                    else
+                                    {
+                                        ?>
+                                            <label for="file-chosen">upload failed</label>
+                                        <?php
+                                    }
+                                }
+                                else
+                                {
+                                    foreach($errors as $value)
+                                    {
+                                ?>
+                                        <br>
+                                        <label for="actual-btn" style="color:red"><?php echo "$value"?></label>
+                                <?php
+                                    }
+                                }
+                            }
+                    ?>
                         </td>
                     </tr>
                 
                 </table>
+            
                 <div class="text-center">
-                    <input type="submit" name="Submit" class="btn btn-info" value="Submit">
+                    <input type="Submit" name ="Submit" class="btn btn-info" value="Submit">
                 </div>
             </form>
         </div>
