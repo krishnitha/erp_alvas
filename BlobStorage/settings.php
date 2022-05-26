6b8991b8217a419246f4ca4a859a2e43291cb8c7
krishnitha <krishnithashetty@gmail.com>
2022-05-26 22:21:01 +0530

    init

--- BlobStorage/settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-$settings = array(
-    "container" => "pdf",
-    "protocol" => "http",
-    "account_name" => "erpalvasblob",
-    "account_key" => "lAHuOOM+ppDGaitJsOAYpTzHpcEOtzeKwOfDJQ+ZZG2BqaEF3nxOJowpAIUN+Ir3jWGwofy8cQ3znzEb1PTkog=="
-);
-?>
